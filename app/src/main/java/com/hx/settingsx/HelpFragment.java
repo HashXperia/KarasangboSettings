cfa3444821b86f6f406deb05fbafc9c4c7dc2166
Abhinav Jhanwar <abhinav.jhanwar.august2@gmail.com>
2015-04-29 20:23:59 +0530

    Add option to rate on google plus

--- app/src/main/java/com/hx/settingsx/HelpFragment.java
+++ app/src/main/java/com/hx/settingsx/HelpFragment.java
@@ -1,11 +1,14 @@
 package com.hx.settingsx;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 public class HelpFragment extends Fragment {
 
@@ -15,6 +18,17 @@ public class HelpFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_help, container, false);
+
+        RelativeLayout rating =(RelativeLayout) rootView.findViewById(R.id.rate);
+        rating.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Uri uri = Uri.parse("https://play.google.com/store/apps/details?id=com.hx.settingsx");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
         return rootView;
     }
 }
