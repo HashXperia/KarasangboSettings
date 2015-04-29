5b11c97a6ed9deb1feaee2272db422c57f275656
Geetesh Kalakoti <kalakotig@gmail.com>
2015-04-29 20:01:02 +0530

    Add first settings option and other things

--- app/src/main/java/com/hx/settingsx/SettingsFragment.java
+++ app/src/main/java/com/hx/settingsx/SettingsFragment.java
@@ -1,20 +1,39 @@
 package com.hx.settingsx;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragment {
 
-    public SettingsFragment() {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.fragment_settings);
+
+        ListPreference listPreference = (ListPreference) findPreference("default_category");
+        listPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                getActivity().getSharedPreferences("com.hx.settingsx", Context.MODE_PRIVATE).edit().putInt("default_category", Integer.valueOf((String) newValue)).commit();
+                return true;
+            }
+        });
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+
+        if (rootView != null) {
+            rootView.setBackgroundColor(getResources().getColor(R.color.grey50));
+        }
+
         return rootView;
     }
 }
